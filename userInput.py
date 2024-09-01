d06e8deeedb6c98a0c7093f6d8d487945d2845c2
kamrulhaider <kamrulhaider.ssb@gmail.com>
2024-09-02 00:52:29 +0600

    User input exercise

---
+++ userInput.py
@@ -0,0 +1,26 @@
+# input = input are the system of taking input from the user. it takes as string
+
+# name = input("What is your name?: ")
+# age = input("What is your age?: ")
+
+
+# print(f"My name is {name} and I am {age} years old.")
+
+# Exercise 1: Rectangle area calculation
+
+# width = float(input("Provide the width: "))
+# height = float(input("Provide the height: "))
+
+# area = width * height
+
+# print(area)
+
+# Excercise 2: Shopping cart example
+
+item = input("What item do you like to buy?: ")
+price = float(input("What is the price of your item?: "))
+quantity = int(input("How many do you want to buy?: "))
+
+total = price * quantity
+
+print(f"You have bought {item}. Total price is {total} Tk")
