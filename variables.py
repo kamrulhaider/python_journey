2c9484237dca4b1e71c45379ee728bc13f2e45ba
kamrulhaider <kamrulhaider.ssb@gmail.com>
2024-09-01 10:58:50 +0600

    Learning variable

---
+++ variables.py
@@ -0,0 +1,16 @@
+# String 
+first_name = "Kamrul"
+
+# f-string literals 
+print(f"Hello {first_name}")
+
+# integer 
+age = 25
+
+print(f"My name is {first_name} and my age is {age}.")
+
+# float 
+gpa = 3.81
+
+# boolean 
+isStudent = True
