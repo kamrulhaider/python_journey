faa268ca30641e92f83262f62003880929a7109d
kamrulhaider <kamrulhaider.ssb@gmail.com>
2024-09-04 19:49:57 +0600

    Madlibs python

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+# Madlibs game
+# Word game where you create a story by filling in gaps with random words
+
+adj1 = input("Enter an adjective: ")
+noun1 = input("Enter an noun: ")
+adj2 = input("Enter an adjective: ")
+verb1 = input("Enter an verb with ing: ")
+adj3 = input("Enter an adjective: ")
+
+print(f"Today i went to a {adj1} zoo.")
+print(f"In an exhibit, I saw a {noun1}.")
+print(f"{noun1} was {adj2} and {verb1}")
+print(f"I was {adj3}")
