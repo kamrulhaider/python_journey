afa7519db894fa9a50d93a167bd161b1ba8895b6
kamrulhaider <kamrulhaider.ssb@gmail.com>
2024-09-04 22:41:02 +0600

    build in math fuction

--- arithmetic.py
+++ arithmetic.py
@@ -1,4 +1,4 @@
-friends = 5
+# friends = 5
 
 # Basic 4 operation
 # friends += 1
@@ -12,7 +12,22 @@ friends = 5
 
 # Modulus
 
-friends %= 2
+# friends %= 2
 
 
-print(friends)
+# print(friends)
+
+
+# Buil in math related function
+
+# x = 3.14
+# y = -4
+# z = 5
+
+# result = round(x)
+# result = abs(y) # absolute value
+# result = pow(4, 3)  # power
+# result = max(x, y, z) # maximum value
+# result = min(x, y, z) # minimum value
+
+# print(result)
