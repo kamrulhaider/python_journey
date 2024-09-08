3342c400e7afa9a91f9b07699ec75fa772de318e
Kamrul Haider Chowdhury <kamrulhaider.ssb@gmail.com>
2024-09-08 23:15:45 +0600

    Ef else example

--- ifstatement.py
+++ ifstatement.py
@@ -1,12 +1,19 @@
 # How to do if else in python
 
-age = int(input("Enter your age: "))
+# age = int(input("Enter your age: "))
 
-if age >= 18:
-    print("You are now signed up.")
-elif age < 0:
-    print("You are not born yet.")
-elif age > 50:
-    print("You are too old to sign up.")
+# if age >= 18:
+#     print("You are now signed up.")
+# elif age < 0:
+#     print("You are not born yet.")
+# elif age > 50:
+#     print("You are too old to sign up.")
+# else:
+#     print("You are not 18+")
+
+response = input("Do you like pet? (Y/N): ")
+
+if response == "Y":
+    print("Thank you for liking us.")
 else:
-    print("You are not 18+")
+    print("You should like pet")
