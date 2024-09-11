dc4e873ef6f25339cf7c9ad09ab99a5116765cd9
Kamrul Haider Chowdhury <kamrulhaider.ssb@gmail.com>
2024-09-11 19:32:14 +0600

    Logical operators and

---
+++ logical-operators.py
@@ -0,0 +1,13 @@
+# Logical Oparators: Logical Oparators are used to evaluate multiple logical conditon expressions.
+# in pythons operators are and, or, not
+
+# Example: We will allow the boy to play outside if his age is more than 30 years and is fit to play outside.
+
+age = 35
+isFitToPlay = True
+
+if age > 30 and isFitToPlay:
+    print("You are allowed to play outside.")
+    print("Let's go!")
+else:
+    print("You are not allowed to play outside.")
