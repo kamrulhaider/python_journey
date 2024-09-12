d03aea0f87fffd6ce051ff1ff9b0454437da47a6
Kamrul Haider Chowdhury <kamrulhaider.ssb@gmail.com>
2024-09-12 15:18:21 +0600

    Not operator

--- logical-operators.py
+++ logical-operators.py
@@ -14,10 +14,21 @@
 
 # Example: We will allow the boy to play outside if his age is more than 30 years or if he is a student.
 
-age = 35
-isStudent = True
+# age = 35
+# isStudent = True
 
-if age > 30 or isStudent:
+# if age > 30 or isStudent:
+#     print("You are allowed to play outside.")
+#     print("Let's go!")
+# else:
+#     print("You are not allowed to play outside.")
+
+# Example: We will allow the boy to play outside if there is no rainfall and he is a student
+
+isStudent = True
+isRaining = False
+
+if isStudent and not isRaining:
     print("You are allowed to play outside.")
     print("Let's go!")
 else:
